b2f9a4f35eb9ee43b28503f32ef079ccac348275
Vladimir Barakhtyanskiy <barahtasurgut@gmail.com>
2024-03-21 14:08:13 +0500

    login comlited

--- client/src/components/MainpageBoard.js
+++ client/src/components/MainpageBoard.js
@@ -8,8 +8,50 @@ export default function MainpageBoard(){
             <div className="backpage_center">
                 <div className="backpage_center_radius"></div>
             </div>
-            <div className="backpage_right"></div>
+            <div className="backpage_right">
+
+            </div>
             <div className="backimg"></div>
+            <div className="backpage-work">
+                <div className="backpage-work_left">
+                    <div className="backpage-work_left_signin">
+                        <div className="backpage-work_left_signin_logo"></div>
+                        <input className="backpage-work_left_signin_input" type="text" placeholder="Логин"/>
+                        <input className="backpage-work_left_signin_input" type="password" placeholder="Пароль"/>
+                        <Link to="/workpage" className="backpage-work_left_signin_btn">ВОЙТИ</Link>
+                    </div>
+                </div>
+                <div className="backpage-work_center"></div>
+                <div className="backpage-work_right">
+                    <div className="backpage-work_right_white">
+                        <div className="backpage-work_right_white_questions">
+                            <div className="backpage-work_right_white_questions_quest">Есть ли необходимость в инструктаже
+                                и проверке знаний сотрудников?</div>
+                            <div className="backpage-work_right_white_questions_checkbox">
+                                <div className="b-w-right-w-q_checkbox_strock">
+                                    <div className="b-w-right-w-q_checkbox_strock_radio">
+                                        <div className="b-w-right-w-q_checkbox_strock_radio_active coloractive"></div>
+                                    </div>
+                                    <div className="b-w-right-w-q_checkbox_strock_option ">Да, конечно</div>
+                                </div>
+                                <div className="b-w-right-w-q_checkbox_strock">
+                                    <div className="b-w-right-w-q_checkbox_strock_radio">
+                                        <div className="b-w-right-w-q_checkbox_strock_radio_active"></div>
+                                    </div>
+                                    <div className="b-w-right-w-q_checkbox_strock_option">Возможно</div>
+                                </div>
+                                <div className="b-w-right-w-q_checkbox_strock">
+                                    <div className="b-w-right-w-q_checkbox_strock_radio">
+                                        <div className="b-w-right-w-q_checkbox_strock_radio_active"></div>
+                                    </div>
+                                    <div className="b-w-right-w-q_checkbox_strock_option">Нет, сомневаюсь</div>
+                                </div>
+                            </div>
+                            <div className="backpage-work_right_white_questions_btn">ОТПРАВИТЬ</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
