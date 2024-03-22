c6276495f807baed2774e3c3395f0b590404ac08
Vladimir Barakhtyanskiy <barahtasurgut@gmail.com>
2024-03-22 16:59:14 +0500

    edwsr

--- client/src/components/CreateTask.js
+++ client/src/components/CreateTask.js
@@ -27,55 +27,24 @@ export default function CreateTask(){
                         <div className="questboard_white_btn" onClick={() => setQuest(3)}>ОПРОС</div>
                         <div className="questboard_white_btn red">ГЛАВНАЯ</div>
                     </div>
-                    
-                }
+
+
                 </div>
                 }
                 {quest === 1 && <div className="questboard_doc">
                     <div className="questboard_doc_title">
                         Создаем новый документ инструктажа...
                     </div>
-                        <div className="quest_title">
-                            <div className="quest_title_left">
-                                <div className="quest_title_left_theme">Тестирование</div>
-                                <div className="quest_title_left_name">Техника безопасности на предприятии</div>
-                            </div>
-                            <div className="quest_title_right">
-                                <div className="quest_title_right_in">Создано: 24.03.2024</div>
-                                <div className="quest_title_right_to">Ознакомиться до: 25.03.2024</div>
-                            </div>
+                    <div className="questboard_doc_create">
+                        <input className="questboard_doc_create_name" placeholder="Введите название Инструктажа" />
+                        <input className="questboard_doc_create_days" placeholder="Дней для ознакомления" type="number"/>
+                        <textarea className="questboard_doc_create" >Описание и указания к ознакомлению</textarea>
+                        <div className="questboard_doc_create_docs">
+                            <div className="questboard_doc_create_docs_pdf"></div>
+                            <div className="questboard_doc_create_docs_plus"></div>
                         </div>
-                        <div className="quest">
-                            <div className="backpage-work_right_white_questions">
-                                <div className="backpage-work_right_white_questions_quest">Есть ли необходимость в инструктаже
-                                    и проверке знаний сотрудников?</div>
-                                <div className="backpage-work_right_white_questions_checkbox">
-                                    <div className="b-w-right-w-q_checkbox_strock">
-                                        <div className="b-w-right-w-q_checkbox_strock_radio">
-                                            <div className="b-w-right-w-q_checkbox_strock_radio_active coloractive"></div>
-                                        </div>
-                                        <div className="b-w-right-w-q_checkbox_strock_option ">Да, конечно</div>
-                                    </div>
-                                    <div className="b-w-right-w-q_checkbox_strock">
-                                        <div className="b-w-right-w-q_checkbox_strock_radio">
-                                            <div className="b-w-right-w-q_checkbox_strock_radio_active"></div>
-                                        </div>
-                                        <div className="b-w-right-w-q_checkbox_strock_option">Возможно</div>
-                                    </div>
-                                    <div className="b-w-right-w-q_checkbox_strock">
-                                        <div className="b-w-right-w-q_checkbox_strock_radio">
-                                            <div className="b-w-right-w-q_checkbox_strock_radio_active"></div>
-                                        </div>
-                                        <div className="b-w-right-w-q_checkbox_strock_option">Нет, сомневаюсь</div>
-                                    </div>
-                                </div>
-                                <div className="backpage-work_right_white_questions_btns">
-                                    <div className="backpage-work_right_white_questions_btn_red">НАЗАД</div>
-                                    <div className="backpage-work_right_white_questions_btn_green">ДАЛЕЕ</div>
-                                </div>
 
-                            </div>
-                        </div>
+                    </div>
 
                 </div>
                 }
